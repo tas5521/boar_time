29f5beafc9a7ffb6feaf8e9ab9c816cfa482ee15
Tas5521 <tsbs11152@gmail.com>
2025-12-06 04:50:23 +0900

    見回り画面作成

--- lib/view/patrol_time/patrol_time_view.dart
+++ lib/view/patrol_time/patrol_time_view.dart
@@ -1,19 +1,101 @@
+import 'package:boar_time/view/view_parts/show_year_month_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 
 class PatrolTimeView extends HookConsumerWidget {
   const PatrolTimeView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final now = DateTime.now();
+    final year = useState(now.year);
+    final month = useState(now.month);
+
+    // 月末の日数を取得
+    final int lastDay = DateTime(year.value, month.value + 1, 0).day;
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          showYearMonthPickerButton(
+            context,
+            onPressed: () async {
+              final now = DateTime.now();
+              final result = await showYearMonthPicker(
+                context,
+                now.year,
+                now.month,
+              );
+              if (result != null) {
+                year.value = result['year']!;
+                month.value = result['month']!;
+              }
+            },
+          ),
+        ],
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('見回り時間'),
+        title: Text('見回り時間', style: TextStyle(fontWeight: FontWeight.bold)),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [Text('見回り時間')],
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.fromLTRB(50.0, 12.0, 12.0, 12.0),
+              color: Colors.orangeAccent,
+              child: Row(
+                children: [
+                  Text('日付', style: TextStyle(fontSize: 16.sp)),
+                  SizedBox(width: 75.w),
+                  Text('開始', style: TextStyle(fontSize: 16.sp)),
+                  SizedBox(width: 49.w),
+                  Text('終了', style: TextStyle(fontSize: 16.sp)),
+                  SizedBox(width: 47.w),
+                  Text('累計', style: TextStyle(fontSize: 16.sp)),
+                ],
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: DataTable(
+                  headingRowHeight: 0,
+                  columnSpacing: 36.w,
+                  columns: const [
+                    DataColumn(label: SizedBox()),
+                    DataColumn(label: SizedBox()),
+                    DataColumn(label: SizedBox()),
+                    DataColumn(label: SizedBox()),
+                  ],
+                  rows: List.generate(lastDay, (i) {
+                    final day = i + 1;
+                    final date = DateTime(year.value, month.value, day);
+                    final formattedDate = DateFormat('yyyy/MM/dd').format(date);
+                    return DataRow(
+                      cells: [
+                        DataCell(
+                          Text(
+                            formattedDate,
+                            style: TextStyle(fontSize: 16.sp),
+                          ),
+                        ),
+                        DataCell(
+                          Text('08:00', style: TextStyle(fontSize: 16.sp)),
+                        ),
+                        DataCell(
+                          Text('17:00', style: TextStyle(fontSize: 16.sp)),
+                        ),
+                        DataCell(
+                          Text('08:00', style: TextStyle(fontSize: 16.sp)),
+                        ),
+                      ],
+                    );
+                  }),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
