66ec2cf979e02836b5ae4c140d137c318d690a9e
Tas5521 <tsbs11152@gmail.com>
2025-12-11 23:12:20 +0900

    ndkVersionを修正

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.gmail.tsbs11152.boar_time"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
