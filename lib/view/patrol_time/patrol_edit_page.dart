f16f20ed9bfe465a9baa7ccea6318eab8bb20b85
Tas5521 <tsbs11152@gmail.com>
2026-01-12 22:59:41 +0900

    データ削除時、stampingNotifierフェッチ

--- lib/view/patrol_time/patrol_edit_page.dart
+++ lib/view/patrol_time/patrol_edit_page.dart
@@ -1,6 +1,7 @@
 import 'package:boar_time/model/patrol_label.dart';
 import 'package:boar_time/model/patrol_record/patrol_record.dart';
 import 'package:boar_time/notifier/patrol/patrol_time_notifier.dart';
+import 'package:boar_time/notifier/stamping/stamping_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -97,6 +98,7 @@ class PatrolEditPage extends HookConsumerWidget {
       if (result != true) return;
 
       await notifier.delete(patrolId: patrolId, year: year, month: month);
+      await ref.read(stampingNotifierProvider.notifier).fetch();
 
       if (context.mounted) {
         Navigator.of(context).pop();
